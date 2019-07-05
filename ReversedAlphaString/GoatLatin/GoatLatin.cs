78679cdc7563486040d3429cd8eb97715d720394
Humaniuc <humancoria@gmail.com>
2019-07-05 11:56:50 +0300

    GoatLatinTests

---
+++ ReversedAlphaString/GoatLatin/GoatLatin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoatLatin
+{
+    public class GoatLatin
+    {
+        enum vowels { a, e, i, o, u };
+        public string GoatLatinSentence(string str)
+        {
+            string[] words = str.Split((char)32);
+
+            StringBuilder strBuild = new StringBuilder();
+            int index = 0;
+
+            foreach(string word in words)
+            {
+                index++;
+                if (Enum.GetNames(typeof(vowels)).Contains(word.Substring(0, 1)))
+                {
+                    strBuild.Append(word + "ma" + string.Empty.PadRight(index, 'a') + " ");
+                }
+                else
+                {
+                    strBuild.Append(word.Substring(1, word.Length - 1) + word[0] + "ma" + string.Empty.PadRight(index, 'a') + " ");
+                }
+            }
+
+            string s = strBuild.ToString().TrimEnd();
+
+            return s;
+        }
+    }
+}
---
+++ ReversedAlphaString/GoatLatin/SentenceTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GoatLatin
+{
+    
+    [TestFixture]
+    class SentenceTests
+    {
+        GoatLatin goat;
+        [SetUp]
+        public void Init()
+        {
+            goat = new GoatLatin();
+        }
+        [TestCase("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa")]
+        [TestCase("The quick brown fox jumped over the lazy dog", "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa")]
+        public void CheckSentence(string s, string expected)
+        {
+            string actual = goat.GoatLatinSentence(s);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
