9f7e00cf00106ec63066258f8da460f482f4b577
Humaniuc <humancoria@gmail.com>
2019-07-05 07:45:50 +0300

    ReversedSpecialStringTests

---
+++ ReversedAlphaString/ReversedAlphaString/StringUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedAlphaString
+{
+    public class StringUtils
+    {
+        public string ReverseUsingStringBuilder(string s)
+        {
+            if (!String.IsNullOrEmpty(s))
+            {
+                StringBuilder strRev = new StringBuilder();
+                int right = s.Length - 1;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (Char.IsLetter(s[i]))
+                    {
+                        while (!Char.IsLetter(s[right]))
+                        {
+                            right--;
+                        }
+                        strRev.Append(s[right]);
+                        right--;
+                    }
+                    else
+                    {
+                        strRev.Append(s[i]);
+                    }
+                }
+                return strRev.ToString();
+            }
+            else
+            {
+                throw new ArgumentNullException("Null or empty");
+            }
+        }
+    }
+}
---
+++ ReversedAlphaString/ReversedAlphaString/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ReversedAlphaString
+{
+    [TestFixture]
+    class Tests
+    {
+        StringUtils stringUtils; 
+        [SetUp]
+        public void Init()
+        {
+            stringUtils = new StringUtils();
+        }
+        [TestCase("ab-cd", "dc-ba")]
+        [TestCase("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
+        [TestCase("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
+        public void CheckReversed(string s, string expected)
+        {
+            var actual = stringUtils.ReverseUsingStringBuilder(s);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void CheckEmptyOrNullString()
+        {
+            string actual = "";
+            Assert.Throws<ArgumentNullException>(() => stringUtils.ReverseUsingStringBuilder(actual));
+        }
+    }
+}
